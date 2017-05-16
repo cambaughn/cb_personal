2810c87e6ea639a640697668171347dff03a13c2
Cameron Baughn <cam.baughn@gmail.com>
2017-05-15 20:16:33 -0700

    Add Home component and Routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 import Navbar from './Navbar';
+import Home from './Home';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+        <Route exact={true} path={`/`} component={Home}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+
+const Home = () => {
+
+  return (
+    <div>
+      <h1> Hello there, </h1>
+    <h3> I'm a front end engineer and product manager. </h3>
+    </div>
+  )
+}
+
+
+export default Home;
