559f34b8724eda255059d400878fd67bb2b0b696
Cameron Baughn <cam.baughn@gmail.com>
2017-05-15 19:49:00 -0700

    Set up Navbar and Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Yo</p>
+        <Navbar />
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+
+const Navbar = () => {
+
+return (
+  <Router>
+    <div style={styles.nav} >
+      <Link to={'/'} style={styles.brand}>
+        <h1> CAMERON BAUGHN </h1>
+      </Link>
+
+      <div style={styles.linkWrapper}>
+        <Link to={'/'} style={styles.link}>
+          home
+        </Link>
+
+        <Link to={'/work'} style={styles.link}>
+          work
+        </Link>
+
+        <Link to={'/contact'} style={styles.link}>
+          contact
+        </Link>
+      </div>
+    </div>
+  </Router>
+)
+
+}
+
+const styles = {
+  nav: {
+    height: 80,
+    minWidth: 600,
+
+    backgroundColor: 'white',
+
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    paddingLeft: 80,
+    paddingRight: 100,
+
+  },
+
+  brand: {
+    fontFamily: 'Oswald',
+    fontWeight: '700',
+
+    color: '#E32F86',
+
+    textDecoration: 'none',
+  },
+
+  linkWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
+    width: 300,
+
+  },
+
+  link: {
+    fontFamily: 'Roboto Condensed',
+    fontWeight: '700',
+
+    textDecoration: 'none',
+    color: 'black',
+    cursor: 'pointer',
+
+  },
+
+}
+
+export default Navbar;
